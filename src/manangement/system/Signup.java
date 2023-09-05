9076abbf5b7fde5ba361b8a3f36ba25b6754ad2a
Muhammad Humza Uddin <thereal.humza@gmail.com>
2023-09-05 13:56:09 +0500

    Add files via upload

--- src/manangement/system/Signup.java
+++ src/manangement/system/Signup.java
@@ -58,16 +58,52 @@ public class Signup extends JFrame{
 		tfFirstname.setBorder(BorderFactory.createEmptyBorder());
 		panel2.add(tfFirstname);
 		
-		JLabel lblLastname = new JLabel("Username");
-		lblLastname.setBounds(80, 90, 200, 30);
+		JLabel lblLastname = new JLabel("Last name");
+		lblLastname.setBounds(80, 210, 200, 30);
 		lblLastname.setFont(new Font("SAN SERIF", Font.PLAIN,14));
 		panel2.add(lblLastname);
 		
 		JTextField tfLastname = new JTextField();
-		tfLastname.setBounds(80, 120, 200, 30);
+		tfLastname.setBounds(80, 240, 200, 30);
 		tfLastname.setBackground(new Color(235, 236, 240));
 		tfLastname.setBorder(BorderFactory.createEmptyBorder());
 		panel2.add(tfLastname);
+		
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setBounds(80, 270, 200, 30);
+		lblEmail.setFont(new Font("SAN SERIF", Font.PLAIN,14));
+		panel2.add(lblEmail);
+		
+		JTextField tfEmail = new JTextField();
+		tfEmail.setBounds(80, 300, 200, 30);
+		tfEmail.setBackground(new Color(235, 236, 240));
+		tfEmail.setBorder(BorderFactory.createEmptyBorder());
+		panel2.add(tfEmail);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(80, 330, 200, 30);
+		lblPassword.setFont(new Font("SAN SERIF", Font.PLAIN,14));
+		panel2.add(lblPassword);
+		
+		JTextField tfPassword = new JTextField();
+		tfPassword.setBounds(80, 360, 200, 30);
+		tfPassword.setBackground(new Color(235, 236, 240));
+		tfPassword.setBorder(BorderFactory.createEmptyBorder());
+		panel2.add(tfPassword);
+		
+		JButton create = new JButton("Create");
+		create.setBackground(new Color(62, 162, 255));
+		create.setBorder(BorderFactory.createEmptyBorder());
+		create.setForeground(Color.WHITE);
+		create.setBounds(80,410,200,30);
+		panel2.add(create);
+		
+		JButton back = new JButton("Back");
+		back.setBackground(new Color(62, 162, 255));
+		back.setBorder(BorderFactory.createEmptyBorder());
+		back.setForeground(Color.WHITE);
+		back.setBounds(80,460,200,30);
+		panel2.add(back);
 		
 		
 		setVisible(true);
