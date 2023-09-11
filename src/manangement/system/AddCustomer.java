349d9e9299a2d0db97b70d7711bbc5aafc46a914
Muhammad Humza Uddin <thereal.humza@gmail.com>
2023-09-12 02:56:28 +0500

    Add files via upload

--- src/manangement/system/AddCustomer.java
+++ src/manangement/system/AddCustomer.java
@@ -8,7 +8,10 @@ import java.awt.*;
 
 public class AddCustomer extends JFrame{
 
-	JLabel lblusername,labelusername,lblId;
+	JLabel lblusername,labelusername, lblId, lblnumber, lblName,labelName;
+	JTextField tfNumber;
+	JComboBox comboId;
+	JRadioButton rmale, rfemale;
 	
 	AddCustomer(){
 		setBounds(200,100,850,550);
@@ -17,7 +20,7 @@ public class AddCustomer extends JFrame{
 		
 		JPanel panel2 = new JPanel();
 		panel2.setBackground(new Color(255,255,255));
-		panel2.setBounds(0,0,350,550);
+		panel2.setBounds(0,0,500,550);
 		panel2.setLayout(null);
 		add(panel2);	
 		
@@ -31,10 +34,54 @@ public class AddCustomer extends JFrame{
 		panel2.add(labelusername);
 		
 		lblId = new JLabel("ID");
-		lblId.setBounds(30, 90, 200, 30);
+		lblId.setBounds(30, 90, 150, 30);
 		lblId.setFont(new Font("SAN SERIF", Font.PLAIN,14));
 		panel2.add(lblId);
 
+		comboId = new JComboBox(new String[] { "Passport", "CNIC Card", "Medical Card", "Employment/Student Card"});
+		comboId.setBounds(220, 90, 200, 30);
+		comboId.setBackground(Color.WHITE);
+		comboId.setFont(new Font("SAN SERIF", Font.PLAIN,14));
+		panel2.add(comboId);
+		
+		lblnumber = new JLabel("Number");
+		lblnumber.setBounds(30, 130, 150, 30);
+		lblnumber.setFont(new Font("SAN SERIF", Font.PLAIN,14));
+		lblnumber.setForeground(Color.BLACK);
+		panel2.add(lblnumber);
+		
+		tfNumber = new JTextField();
+		tfNumber.setBounds(220, 130, 200, 30);
+		tfNumber.setFont(new Font("SAN SERIF", Font.PLAIN,14));
+		panel2.add(tfNumber);
+		
+		lblName = new JLabel("Name");
+		lblName.setBounds(30, 170, 150, 30);
+		lblName.setFont(new Font("SAN SERIF", Font.PLAIN,14));
+		panel2.add(lblName);
+		
+		labelName = new JLabel();
+		labelName.setBounds(220, 170, 150, 30);
+		panel2.add(labelName);
+		
+		JLabel lblGender = new JLabel("Gender");
+		lblGender.setBounds(30, 210, 150, 30);
+		lblGender.setFont(new Font("SAN SERIF", Font.PLAIN,14));
+		panel2.add(lblGender);
+		
+		rmale = new JRadioButton("Male");
+		rmale.setBounds(220, 210, 70, 30);
+		rmale.setBackground(Color.WHITE);
+		panel2.add(rmale);
+		
+		rfemale = new JRadioButton("Female");
+		rfemale.setBounds(300, 210, 70, 30);
+		rfemale.setBackground(Color.WHITE);
+		panel2.add(rfemale);
+		
+		ButtonGroup bg = new ButtonGroup();
+		bg.add(rmale);
+		bg.add(rfemale);
 		
 		setVisible(true);
 	}
